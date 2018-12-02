a2409b479aba33831bb4d38131362d66e40745c1
dengzj <dengzj@canplay.com.cn>
2018-12-03 00:46:00 +0800

    添加 GPUImage

--- ZJShortVideo/Modules/Video/View/ZJCaptureVideoView.swift
+++ ZJShortVideo/Modules/Video/View/ZJCaptureVideoView.swift
@@ -11,28 +11,15 @@ import GPUImage
 
 class ZJCaptureVideoView: UIView {
 
-    var videoCamera: GPUImageVideoCamera? = {
+    lazy var videoCamera: GPUImageVideoCamera = {
         let videoCamera = GPUImageVideoCamera(sessionPreset: AVCaptureSession.Preset.hd1280x720.rawValue, cameraPosition: .front)
-//        if try! videoCamera?.inputCamera.lockForConfiguration() != nil {
-//            //自动对焦
-//            if (videoCamera?.inputCamera.isFocusModeSupported(.continuousAutoFocus))! {
-//                videoCamera?.inputCamera.focusMode = .continuousAutoFocus
-//            }
-//            //自动曝光
-//            if (videoCamera?.inputCamera.isExposureModeSupported(.continuousAutoExposure))! {
-//                videoCamera?.inputCamera.exposureMode = .continuousAutoExposure
-//            }
-//            //自动白平衡
-//            if (videoCamera?.inputCamera.isWhiteBalanceModeSupported(.continuousAutoWhiteBalance))! {
-//                videoCamera?.inputCamera.whiteBalanceMode = .continuousAutoWhiteBalance
-//            }
-//            videoCamera?.inputCamera.unlockForConfiguration()
-//        }
+
+        
         videoCamera?.outputImageOrientation = .portrait
         videoCamera?.addAudioInputsAndOutputs()
         videoCamera?.horizontallyMirrorRearFacingCamera = false
         videoCamera?.horizontallyMirrorFrontFacingCamera = true
-        return videoCamera
+        return videoCamera!
     }()
     var filter: GPUImageFilter?
     
@@ -43,8 +30,21 @@ class ZJCaptureVideoView: UIView {
     }
     
     func setUpVideoCamera() {
-    
-        videoCamera?.startCapture()
+        try? videoCamera.inputCamera.lockForConfiguration()
+        // 自动对焦
+        if (videoCamera.inputCamera.isFocusModeSupported(.autoFocus)) {
+            videoCamera.inputCamera.focusMode = .continuousAutoFocus
+        }
+        //自动曝光
+        if (videoCamera.inputCamera.isExposureModeSupported(.autoExpose)) {
+            videoCamera.inputCamera.exposureMode = .continuousAutoExposure
+        }
+        //自动白平衡
+        if (videoCamera.inputCamera.isWhiteBalanceModeSupported(.autoWhiteBalance)) {
+            videoCamera.inputCamera.whiteBalanceMode = .continuousAutoWhiteBalance
+        }
+        videoCamera.inputCamera.unlockForConfiguration()
+        videoCamera.startCapture()
     }
     
     required init?(coder aDecoder: NSCoder) {
