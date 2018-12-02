b4600a48f72c57458f26f1c063db51069272dbc7
dengzj <dengzj@canplay.com.cn>
2018-12-02 14:15:41 +0800

    first push ZJShortVideo

---
+++ ZJShortVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZJShortVideo
+//
+//  Created by 邓志坚 on 2018/12/2.
+//  Copyright © 2018 邓志坚. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
